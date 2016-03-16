f52de61f0709e4737b529760b8e663ddf81ac20b
Unnar Þór Axelsson <unnarthor@outlook.com>
2016-03-17 00:46:28 +0100

    Able to create request token and get a responce

--- smugUploader/MainPage.xaml.cs
+++ smugUploader/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using smugUploader.OAuth;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,57 @@ namespace smugUploader
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private static async Task<string> GetResponseContent(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await (await client.GetAsync(url)).Content.ReadAsStringAsync();
+            }
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            const string OAuthBaseUrl = "https://secure.smugmug.com";
+            const string OAuthGetRequestTokenUrl = OAuthBaseUrl + "/services/oauth/1.0a/getRequestToken";
+            const string OAuthAuthorizeUrl = OAuthBaseUrl + "/services/oauth/1.0a/authorize";
+            const string OAuthGetAccessTokenUrl = OAuthBaseUrl + "/services/oauth/1.0a/getAccessToken";
+            const string NonWebOAuthApplicationCallback = "oob";
+
+            smugUploader.OAuthAuthenticator authenticator = new smugUploader.OAuthAuthenticator("xxGn8kaV0HfrQ1m8fvrtKYQFcqnAQUvl", "28eb860ff20d1884ec54e5cb9c3dad9a");
+            string reqTokUrl = authenticator.CreateGetRequestTokenAddress(OAuthGetRequestTokenUrl, HttpMethod.Get.ToString(), NonWebOAuthApplicationCallback);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(reqTokUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                textBox.Text = await response.Content.ReadAsStringAsync();
+            }
+
+            //string tokens = GetResponseContent(reqTokUrl).Result;
+            // Figure out which authorization options are requested (if any)
+            //string authorizationOptions = options == null ? string.Empty : "?" + options.AsQueryString();
+            //string authorizeUrl = authenticator.CreateAuthorizeAddress(OAuthAuthorizeUrl, tokens);
+            
+
+            //var uri = new Uri(@authorizeUrl);
+
+            // Launch the URI
+            //var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+
+            //if (success)
+            //{
+            //    textBox.Text = reqTokUrl;
+            //}
+            //else
+            //{
+            //    textBox.Text = "ohhhh";
+            //}
+            //string tokens = GetResponseContent(reqTokUrl).Result;
+            //OAuth.OAuth smugOauth = new OAuth.OAuth("xxGn8kaV0HfrQ1m8fvrtKYQFcqnAQUvl", "28eb860ff20d1884ec54e5cb9c3dad9a");
+            //string normalized = smugOauth.GetAuthTokens();
+
         }
     }
 }
